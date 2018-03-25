42d146acbc627bbf73bf12cc4c2d94d9b898f8a8
banjun <banjun@gmail.com>
2018-03-25 14:26:45 +0900

    initial commit

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '10.0'
+use_frameworks!
+
+target 'PriparaDB-song-ios' do
+  pod 'NorthLayout'
+  pod '※ikemen'
+  pod 'ReactiveCocoa'
+end
---
+++ Pods/Target Support Files/Pods-PriparaDB-song-ios/Pods-PriparaDB-song-ios-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PriparaDB_song_ios : NSObject
+@end
+@implementation PodsDummy_Pods_PriparaDB_song_ios
+@end
